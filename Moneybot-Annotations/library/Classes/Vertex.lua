2c805ff0c15d999c4f154605d7348844cc0c6905
daily <60293305+daily3014@users.noreply.github.com>
2024-03-30 15:15:55 +0100

    fix vertex function descriptions

--- Moneybot-Annotations/library/Classes/Vertex.lua
+++ Moneybot-Annotations/library/Classes/Vertex.lua
@@ -1,15 +1,15 @@
 ---@meta
----@class Vector
+---@class Vertex
 ---@field position Vector
 ---@field texCoord Vector
 local Vertex = {}
 
---- Creates a vector struct
+--- Creates a vertex struct
 ---@return Vertex
 ---@overload fun(position: Vector, texCoord: Vector?): Vector
 ---@overload fun(vertex: { position: Vector, texCoord: Vector? }): Vector
 function _G.vertex() end
 
----Unpacks the vector
+---Unpacks the vertex
 ---@return number pos_x, number pos_y, number vert_x, number vert_y 
 function Vertex:unpack() end
