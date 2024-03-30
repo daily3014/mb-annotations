18dba99b5530562f28e377c821f6d97a548154bc
daily <60293305+daily3014@users.noreply.github.com>
2024-03-30 19:58:45 +0100

    fix json duplicate function name, bitbuffer description removed

--- Moneybot-Annotations/library/Libraries/json.lua
+++ Moneybot-Annotations/library/Libraries/json.lua
@@ -11,6 +11,6 @@ function json.stringify(data) end
 ---Converts json back to a table
 ---@param json_string string
 ---@return table|nil json_representation
-function json.stringify(json_string) end
+function json.parse(json_string) end
 
 return json
